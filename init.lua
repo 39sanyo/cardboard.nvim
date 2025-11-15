5039f21f0da3d67932e3f54486deaaf5a41a8828
Santiago Ochoa <santiagoochoa@Mac.lan>
2025-11-15 10:50:07 -0500

    first commit

---
+++ init.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+M.colors = {
+    bg      = "#1f2424",
+    fg      = "#e5dfd3",
+
+    black   = "#4a5353",
+    red     = "#896d6d",
+    green   = "#6d896d",
+    yellow  = "#89896d",
+    blue    = "#6d6d89", 
+    magenta = "#896d89",
+    cyan    = "#6d8989",
+    white   = "#e5dfd3",
+
+    bright_red = "#a38989",
+    bright_green = "#89a389",
+    bright_yellow = "#a3a389"
+    bright_blue = "#8989a3",
+    bright_magenta = "#a389a3",
+    bright_cyan = "#a9a3a3"
+}
